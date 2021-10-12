1ed4feed2015909ef956c665df0e85f6991a8a8a
rafaalberto02 <rafa.alberto02@gmail.com>
2021-10-11 23:04:33 -0300

    Fix: Language translate. Fix: dbscript on build of the container. Add: User for database

--- dbscript.sql
+++ dbscript.sql
@@ -1,28 +1,38 @@
-create table Endereco (	
-	/*
-        Referência para o comprimento dos caracteres:
-        https://biblioteca.ibge.gov.br/visualizacao/livros/liv101639.pdf
-    */ 
+create table Address (
     id int not null primary key auto_increment,
-    cep varchar(8),
-    logradouro varchar(40),
-    complemento varchar(24),
-    bairro varchar(40),
-    cidade varchar(60),
-    uf varchar(2),
-    numero int(3)
+    zipCode varchar(8),
+    street varchar(40),
+    complement varchar(24),
+    neighbourhood varchar(40),
+    city varchar(60),
+    state varchar(2),
+    number int(4)
 );
 
-create table Imovel (
+create table Immobile (
     id int not null primary key auto_increment,
-    idEndereco int not null,
-    valor double,
-    qtdQuartos int,
-    qtdVagas int,
-    qtdSuites int,
+    idAddress int not null,
+    value double,
+    qtyRooms int,
+    qtyCarSpaces int,
+    qtySuites int,
     area double,
+    rent int(1),
+    condominium double,
     
-    foreign key (idEndereco)
-		references Endereco(id)
+    foreign key (idAddress)
+		references Address(id)
 			on delete cascade on update cascade
 );
+
+insert into Address(zipCode, street, complement, neighbourhood, city, state, number) values ('45657721', 'Rua C', '', 'São Domingos', 'Ilhéus', 'BA', 12);
+insert into Address(zipCode, street, complement, neighbourhood, city, state, number) values ('74926010', 'Rua Ametista', '', 'Jardim Colorado', 'Aparecida de Goiânia', 'GO', 412);
+insert into Address(zipCode, street, complement, neighbourhood, city, state, number) values ('68903082', 'Passagem das Naçôes', '', 'Pedrinhas', 'Macapá', 'AP', 551);
+insert into Address(zipCode, street, complement, neighbourhood, city, state, number) values ('69151537', 'Avenida Nações Unidas', '', 'São Benedito', 'Parintins', 'AM', 6723);
+insert into Address(zipCode, street, complement, neighbourhood, city, state, number) values ('78065428', 'Rua Itália', '', 'Jardim Europa', 'Cuiabá', 'MT', 12);
+
+insert into Immobile(idAddress, value, qtyRooms, qtyCarSpaces, qtySuites, area, rent, condominium) values (1, 3200, 3, 1, 1, 120, 1, 300);
+insert into Immobile(idAddress, value, qtyRooms, qtyCarSpaces, qtySuites, area, rent, condominium) values (2, 900, 1, 1, 0, 38, 1, 80);
+insert into Immobile(idAddress, value, qtyRooms, qtyCarSpaces, qtySuites, area, rent, condominium) values (3, 2400, 2, 2, 1, 98, 1, 250);
+insert into Immobile(idAddress, value, qtyRooms, qtyCarSpaces, qtySuites, area, rent, condominium) values (4, 1200, 2, 1, 0, 49, 1, 120);
+insert into Immobile(idAddress, value, qtyRooms, qtyCarSpaces, qtySuites, area, rent, condominium) values (5, 280000, 3, 2, 1, 200, 0, 500);
