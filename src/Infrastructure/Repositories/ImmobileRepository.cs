ee69d5ac6ced6fc24dd631a7655ea6c79f49a814
rafaalberto02 <rafa.alberto02@gmail.com>
2021-10-17 21:21:15 -0300

    Add: Implemented all related interfaces to the ImmobileRepository and create the function of getting all

--- src/Infrastructure/Repositories/ImmobileRepository.cs
+++ src/Infrastructure/Repositories/ImmobileRepository.cs
@@ -1,28 +1,61 @@
+using System.Data;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain.Interfaces;
 using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Dapper;
 
 namespace Infrastructure.Repositories
 {
     public class ImmobileRepository : IImmobileRepository
     {
-        public Task<bool> delete(int id)
+        private readonly ILogger _logger;
+        private readonly string _connection;
+
+        public ImmobileRepository(IConfiguration config, ILoggerFactory factory)
+        {
+            _connection = config.GetConnectionString("DefaultConnection");
+            _logger = factory.CreateLogger<ImmobileRepository>();
+        }
+
+        public async Task<bool> delete(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<IEnumerable<Immobile>> getAll()
+        public async Task<IEnumerable<Immobile>> getAll()
+        {
+            string sql = "SELECT * FROM Immobile I INNER JOIN Address A WHERE I.idAddress = A.id;";
+
+            try
+            {
+                _logger.LogTrace(_connection);
+                using (var conn = new MySqlConnection(_connection))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    var immobiles = await conn.QueryAsync<Immobile, Address, Immobile>(sql: sql, map: (immobile, address) => { immobile.address = address; return immobile; }, splitOn: "id");
+
+                    return immobiles;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
+        }
+
+        public async Task<Immobile> getById(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<Immobile> getById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Immobile> insert(Immobile immobile)
+        public async Task<Immobile> insert(Immobile immobile)
         {
             throw new System.NotImplementedException();
         }
